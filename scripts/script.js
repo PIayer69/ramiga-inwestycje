4a24f161520e8c4f3a5c5ab26f0968fc332a9222
piayer69 <maxmarsz1@gmail.com>
2023-05-29 18:52:12 +0200

    working on burger

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,29 @@
 
-
-burger = document.querySelector(".burger-menu")
+body = document.body;
+burger = document.querySelector(".burger-menu");
+burgerBars = burger.querySelectors
+cross = document.querySelector(".cross");
 burger.addEventListener("click", toggleMenu);
+// cross.addEventListener("click", toggleMenu);
+sidebar = document.querySelector(".sidebar");
 
 function toggleMenu(){
-    console.log("hello")
+    burger.style = "position: relative; z-index: 1;"
+    if(sidebar.style.display == "" || sidebar.style.display == "none"){
+        body.style.overflow = "hidden";
+        sidebar.style = "right: 0";
+        sidebar.style.display = "flex";
+        console.log(burger.children)
+        bars = Array.from(burger.children)
+        bars.forEach(bar => {
+            bar.style = "transform: rotate(140deg)";
+        });
+        
+        
+    }
+    else{
+        body.style.overflow = "overflow-x";
+        sidebar.style = "right: -500px";
+        sidebar.style.display = "none";
+    }
 }
