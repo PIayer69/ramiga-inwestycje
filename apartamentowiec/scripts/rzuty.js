9b00bb66a92fea15c2ac502c472105685188b623
piayer69 <maxmarsz1@gmail.com>
2023-06-02 14:11:27 +0200

    Added hover and selected effects in apartamentowiec #rzuty

--- apartamentowiec/scripts/rzuty.js
+++ apartamentowiec/scripts/rzuty.js
@@ -9,12 +9,15 @@ let floorOptions = document.querySelector("#floor")
 let flatOptions = document.querySelector("#flat");
 let flatButtons = Array.from(flatOptions.children);
 flatButtons.shift();
+let buttonActive = null;
 let flatActive = null;
 let infoOptions = document.querySelector(".info");
 let infoDivs = Array.from(infoOptions.children);
 infoDivs.shift();
 let infoActive = null;
 let flatIDContainer = document.querySelector("#m_id");
+
+let imageOverlays = Array.from(document.querySelector(".overlay").children);
 
 let buttons = Array.from(document.querySelector("#floor_btns").children);
 buttons.forEach(btn => {
@@ -41,19 +44,47 @@ flatButtons.forEach(btnSet => {
                 if(infoActive != null){
                     infoDivs[infoActive].classList.add("hidden");
                     infoOptions.classList.add("hidden");
+                    toggleSelected(infoActive);
                     infoActive = null;
                 }
             }
             else{
                 if(infoActive != null){
                     infoDivs[infoActive].classList.add("hidden");
+                    toggleSelected(infoActive)
+                    buttonActive.classList.toggle("selected")
                 }
+                buttonActive = btn;
+                btn.classList.toggle("selected")
                 infoOptions.classList.remove("hidden");
                 infoDivs[event.target.id-1].classList.remove("hidden");
                 infoActive = event.target.id - 1;
+                toggleSelected(infoActive)
                 flatIDContainer.innerHTML = infoActive + 1;
 
             }
         })
+        btn.addEventListener('mouseenter', event => {hover(event.target.id-1)})
+        btn.addEventListener('mouseleave', event => {hover(event.target.id-1)})
+
     })
 })
+
+function hover(id){
+    // id = 0 - 10
+    let l = [2, 0, 1, 3];
+    offset = 0;
+    if(id<3) offset = 1;
+
+    imageOverlays[l[(id+1)%4]].classList.toggle("hover");
+}
+
+function toggleSelected(id){
+    let l = [2, 0, 1, 3];
+    offset = 0;
+    if(id<3) offset = 1;
+
+    imageOverlays[l[(id+1)%4]].classList.toggle("selected");
+
+}
+
